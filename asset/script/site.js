a47ff17e5401088cf745317706d014e7929ea628
Rowan Manning <info@rowanmanning.com>
2014-05-15 21:24:23 +0100

    Simplify things with jQuery

--- asset/script/site.js
+++ asset/script/site.js
@@ -1,10 +1,10 @@
-(function () {
+(function ($) {
 
 
     // Document ready
-    document.body.onload = function () {
+    $(function () {
         cuff();
-    };
+    });
 
 
     // UI Controls
@@ -15,20 +15,30 @@
 
             // Initialise the control
             init: function () {
+                this.el = $(el);
+                this.input = this.el.find('[data-role=filter]');
+                this.images = this.el.find('[data-role=image]');
+                this.bindEvents();
+            },
+
+            // Bind DOM events
+            bindEvents: function () {
                 var self = this;
-                this.el = el;
-                this.filterInput = cuff.controls.ImageFilterInput(el.querySelector('[data-role=filter]'), this);
-                this.images = toArray(el.querySelectorAll('[data-role=image]')).map(function (imageEl) {
-                    return cuff.controls.Image(imageEl, self.previewPane);
+                this.input.on('keyup', function () {
+                    self.filter(self.input.val());
                 });
             },
 
             // Filter images by a query string
             filter: function (query) {
                 query = this.buildQueryRegExp(query);
-                this.images.forEach(function (image) {
-                    return image.displayByRegExp(query);
-                });
+                console.log('filter');
+                this.images
+                    .removeClass('hide')
+                    .filter(function () {
+                        return !query.test($(this).data('tags'));
+                    })
+                    .addClass('hide');
             },
 
             // Build a regular expression for a query
@@ -42,93 +52,12 @@
         return control;
     };
 
-    // ImageFilterInput control (not used directly in DOM)
-    cuff.controls.ImageFilterInput = function (el, list) {
-        var control = {
-
-            // Initialise the control
-            init: function () {
-                this.el = el;
-                this.bindEvents();
-            },
-
-            // Bind DOM events
-            bindEvents: function () {
-                el.addEventListener('keyup', function () {
-                    list.filter(el.value);
-                });
-            }
-
-        };
-        control.init();
-        return control;
-    };
-
-    // Image control (not used directly in DOM)
-    cuff.controls.Image = function (el, previewPane) {
-        var control = {
-
-            // Initialise the control
-            init: function () {
-                this.el = el;
-                this.linkEl = el.querySelector('[data-role=url]');
-                this.tags = toArray(el.querySelectorAll('[data-role=tag]')).map(function (tagEl) {
-                    return sanitizeTag(tagEl.innerText);
-                });
-                this.tagString = this.tags.join(' ');
-                this.bindEvents();
-            },
-
-            // Bind DOM events
-            bindEvents: function () {
-                var self = this;
-                this.linkEl.addEventListener('mousedown', function () {
-                    self.preview();
-                    return false;
-                });
-            },
-
-            // Get whether the image matches a RegExp query
-            matchesRegExp: function (query) {
-                return query.test(this.tagString);
-            },
-
-            // Display/hide the image based on whether it matches a RegExp query
-            displayByRegExp: function (query) {
-                if (this.matchesRegExp(query)) {
-                    el.style.display = 'block';
-                } else {
-                    el.style.display = 'none';
-                }
-            },
-
-            // Load a preview image
-            preview: function () {
-                if (!this.hasPreview) {
-                    el.innerHTML += '' +
-                        '<div class="preview">' +
-                            '<img src="' + this.linkEl.href + '" alt="Image preview"/>' +
-                        '</div>';
-                    this.hasPreview = true;
-                }
-            }
-
-        };
-        control.init();
-        return control;
-    };
-
 
     // Utilities
-
-    // Convert array-like object to array
-    function toArray (obj, fn) {
-        return Array.prototype.slice.call(obj);
-    }
 
     // Sanitize a tag
     function sanitizeTag (tag) {
         return tag.replace(/[^a-z0-9\-]+/, '').trim().toLowerCase();
     }
 
-} ());
+} (jQuery));
